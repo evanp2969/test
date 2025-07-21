ba801532069de076845b165c352542f36a0cd3b0
evanp2969 <evanp2969@gmail.com>
2025-07-21 18:31:25 -0400

    Add files via upload
    New main.java Class to replace MyFirstApp.java

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main{
+
+    public static void main(String[] args){
+        System.out.println("Number Sort Program");
+        System.out.println("Loading numbers.txt");
+        //load numbers.txt
+        System.out.println("Welcome to the Number Sort Program, please select sort option from the following menu");
+        //print sort menu
+        //get user input
+    }
+
+
+}
